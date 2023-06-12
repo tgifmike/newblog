ee3e04a8a331e48ac4e1a316197ba4c228fb0564
mike sorrentino <mr.mikesorrentino@gmail.com>
2023-06-12 08:47:28 -0400

    home nav

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,22 @@
+import Link from "next/link";
 import React from "react";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <h1>Welcome to Blog</h1>
+      <ul>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <Link href="/about">About</Link>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default HomePage;
