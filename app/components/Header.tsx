cd9d1aade1964c6e5bd10183df6ea49db3494ca1
mike sorrentino <mr.mikesorrentino@gmail.com>
2023-06-13 05:31:49 -0400

    changed TML to HOME

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
     <div className="flex items-center justify-between">
       <header className="py-5 bg-gray-700 text-white text-center">
         <div className="flex items-center gap-5 text-lg text-gray-600">
-          <Link href="/">TML</Link>
+          <Link href="/">HOME</Link>
           <Link href="/about">About</Link>
         </div>
       </header>
