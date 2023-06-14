fedb31de83e8fef3448b294f0f9c8bb2a8990e7c
mike sorrentino <mr.mikesorrentino@gmail.com>
2023-06-13 22:30:52 -0400

    trailing / in next.config false

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@
 const nextConfig = {
   output: "export",
   // Optional: Add a trailing slash to all paths `/about` -> `/about/`
-  // trailingSlash: true,
+  trailingSlash: false,
   // Optional: Change the output directory `out` -> `dist`
   // distDir: 'dist',
 };
