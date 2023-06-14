1b88bd6f0324586f13eadee8c7211c75158eb2c6
mike sorrentino <mr.mikesorrentino@gmail.com>
2023-06-13 22:06:30 -0400

    chang next-config

--- next.config.js
+++ next.config.js
@@ -10,3 +10,23 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+
+// next.config.js
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = "";
+let basePath = "/";
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+
+  assetPrefix = `/${"https://tgifmike.github.io/newblog/"}/`;
+  basePath = `/${"https://tgifmike.github.io/newblog/"}`;
+}
+
+module.exports = {
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+};
