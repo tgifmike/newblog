06eab2ad35cc41f0a46df3000fd92eedd059b69c
mike sorrentino <mr.mikesorrentino@gmail.com>
2023-06-14 22:24:46 -0400

    add link to pages on home screen

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { getFirstSixPosts } from "@/lib/db_utilitites";
+import Link from "next/link";
 import React from "react";
 
 export default async function HomePage() {
@@ -10,7 +11,8 @@ export default async function HomePage() {
       </h1>
       <div className="mt-5 grid md:grid-cols-2 lg:grid-cols-3 gap-8">
         {sixPosts.map((sixPosts) => (
-          <div
+          <Link
+            href={`/blogs/${sixPosts.slug}`}
             key={sixPosts.id}
             className="border-2 border-grey-500 rounded-lg p-1 hover:scale-105 hover:border-blue-500 transition"
           >
@@ -18,7 +20,7 @@ export default async function HomePage() {
               {sixPosts?.title}
             </h3>
             <p className="text-md text-gray-700 mt-5">{sixPosts?.preview}</p>
-          </div>
+          </Link>
         ))}
       </div>
     </main>
