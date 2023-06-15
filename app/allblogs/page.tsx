83a44d41d331b8314acea67e0ee69c54c4c406ff
mike sorrentino <mr.mikesorrentino@gmail.com>
2023-06-14 20:28:56 -0400

    took div away anf moved key on allblog page

--- app/allblogs/page.tsx
+++ app/allblogs/page.tsx
@@ -8,11 +8,12 @@ export default async function AllblogPage() {
       <h1 className="text-xl text-slate-600">All Blog Posts</h1>
 
       {posts?.map((post) => (
-        <div className="border-2 border-slate-900 rounded-lg p-2 m-2">
-          <div key={post.id}>
-            <h3 className="text-lg text-red-400">{post.title}</h3>
-            <p className="text-md">{post.preview}</p>
-          </div>
+        <div
+          key={post.id}
+          className="border-2 border-slate-900 rounded-lg p-2 m-2"
+        >
+          <h3 className="text-lg text-red-400">{post.title}</h3>
+          <p className="text-md">{post.preview}</p>
         </div>
       ))}
     </div>
