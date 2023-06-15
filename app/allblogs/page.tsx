b05f9eb85c58e1f823a268813c3d5e13aa360164
mike sorrentino <mr.mikesorrentino@gmail.com>
2023-06-14 22:34:47 -0400

    add style and links to all blog page

--- app/allblogs/page.tsx
+++ app/allblogs/page.tsx
@@ -1,21 +1,26 @@
 import { getPosts } from "@/lib/db_utilitites";
+import Link from "next/link";
 import React from "react";
 
 export default async function AllblogPage() {
   const posts = await getPosts();
   return (
-    <div>
+    <main>
       <h1 className="text-xl text-slate-600">All Blog Posts</h1>
-
-      {posts.map((post) => (
-        <div
-          key={post.id}
-          className="border-2 border-slate-900 rounded-lg p-2 m-2 hover:scale-105 hover:border-blue-500 transition"
-        >
-          <h3 className="text-lg text-red-400 font-bold">{post.title}</h3>
-          <p className="text-md">{post.preview}</p>
-        </div>
-      ))}
-    </div>
+      <div className="mt-5 grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+        {posts.map((post) => (
+          <Link
+            href={`/blogs/${post.slug}`}
+            key={post.id}
+            className="border-2 border-slate-900 rounded-lg p-1 m-1 hover:scale-105 hover:border-blue-500 transition"
+          >
+            <h3 className="mt-2 text-lg font-extrabold text-red-400">
+              {post.title}
+            </h3>
+            <p className="text-md">{post.preview}</p>
+          </Link>
+        ))}
+      </div>
+    </main>
   );
 }
