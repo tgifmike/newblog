6e11aab08076fcd8f3e5cd6e171135d8909afa03
mike sorrentino <mr.mikesorrentino@gmail.com>
2023-06-21 16:06:56 -0400

    add ./ to headerfile

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -6,9 +6,9 @@ const Header = () => {
   return (
     <div className="max-w-5xl mx-auto py-1">
       <header className="flex items-center justify-between">
-        <Link href="/">
+        <Link href="./">
           <Image
-            src="/TML_logo.png"
+            src="./TML_logo.png"
             width={250}
             height={200}
             alt="The Manager Life Logo"
