c4e70de7269be537a7a77f9ce4f1177e3c5ac214
mike sorrentino <mr.mikesorrentino@gmail.com>
2023-06-21 16:23:45 -0400

    add ./ to link paths on homepage

--- app/allblogs/page.tsx
+++ app/allblogs/page.tsx
@@ -11,7 +11,7 @@ export default async function AllblogPage() {
       <div className="mt-5 grid md:grid-cols-2 lg:grid-cols-3 gap-8">
         {posts.map((post) => (
           <Link
-            href={`/blogs/${post.slug}`}
+            href={`./blogs/${post.slug}`}
             key={post.id}
             className="border-2 border-slate-900 rounded-lg p-1 m-1 hover:scale-105 hover:border-blue-500 transition"
           >
